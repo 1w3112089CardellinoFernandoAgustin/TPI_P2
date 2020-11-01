63974ff56fafb1ac20e326596f1fba7577892dbd
1W3111894MariniMelisa <111894@tecnicatura.frc.utn.edu.ar>
2020-11-01 16:39:45 -0300

    Update frmReportePelicula.Designer.cs

--- CineShowAPP/CineShowAPP/frmReportePelicula.Designer.cs
+++ CineShowAPP/CineShowAPP/frmReportePelicula.Designer.cs
@@ -31,13 +31,23 @@
             this.components = new System.ComponentModel.Container();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReportePelicula));
-            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.dsTablaPeliComposicion = new CineShowAPP.dsTablaPeliComposicion();
             this.dataTablePeliculasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dsTablaPeliComposicion = new CineShowAPP.dsTablaPeliComposicion();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.dataTablePeliculasTableAdapter = new CineShowAPP.dsTablaPeliComposicionTableAdapters.DataTablePeliculasTableAdapter();
-            ((System.ComponentModel.ISupportInitialize)(this.dsTablaPeliComposicion)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataTablePeliculasBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsTablaPeliComposicion)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataTablePeliculasBindingSource
+            // 
+            this.dataTablePeliculasBindingSource.DataMember = "DataTablePeliculas";
+            this.dataTablePeliculasBindingSource.DataSource = this.dsTablaPeliComposicion;
+            // 
+            // dsTablaPeliComposicion
+            // 
+            this.dsTablaPeliComposicion.DataSetName = "dsTablaPeliComposicion";
+            this.dsTablaPeliComposicion.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // reportViewer1
             // 
@@ -48,23 +58,13 @@
             reportDataSource1.Value = this.dataTablePeliculasBindingSource;
             this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "CineShowAPP.rptPeliculas.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(18, 18);
-            this.reportViewer1.Margin = new System.Windows.Forms.Padding(15, 15, 15, 15);
+            this.reportViewer1.Location = new System.Drawing.Point(12, 12);
+            this.reportViewer1.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(1091, 633);
+            this.reportViewer1.Size = new System.Drawing.Size(728, 412);
             this.reportViewer1.TabIndex = 0;
             this.reportViewer1.ZoomMode = Microsoft.Reporting.WinForms.ZoomMode.PageWidth;
-            // 
-            // dsTablaPeliComposicion
-            // 
-            this.dsTablaPeliComposicion.DataSetName = "dsTablaPeliComposicion";
-            this.dsTablaPeliComposicion.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // dataTablePeliculasBindingSource
-            // 
-            this.dataTablePeliculasBindingSource.DataMember = "DataTablePeliculas";
-            this.dataTablePeliculasBindingSource.DataSource = this.dsTablaPeliComposicion;
             // 
             // dataTablePeliculasTableAdapter
             // 
@@ -72,20 +72,19 @@
             // 
             // frmReportePelicula
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackgroundImage = global::CineShowAPP.Properties.Resources.pngtree_atmospheric_textured_film_and_television_company_propaganda_background_image_127872;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1116, 671);
+            this.ClientSize = new System.Drawing.Size(949, 436);
             this.Controls.Add(this.reportViewer1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmReportePelicula";
             this.Text = "frmReportePelicula";
             this.Load += new System.EventHandler(this.frmReportePelicula_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dsTablaPeliComposicion)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataTablePeliculasBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsTablaPeliComposicion)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
